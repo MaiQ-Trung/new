d5a5097f7255845a85fce106e2fe741c0135021e
MaiQ-Trung <mai.qtrung2601@gmail.com>
2024-09-05 15:30:04 +0700

    5/9

--- Project test/Tree Diagram/src/App.vue
+++ Project test/Tree Diagram/src/App.vue
@@ -1,9 +1,194 @@
 <template>
   <div id="app">
-    <Tree />
+    <div class="flex w-screen h-screen p-20">
+      <UserTree :users="users" :ceoName="users[0].name"/>
+    </div>
   </div>
 </template>
 
 <script setup>
-import Tree from "@/components/Tree.vue";
+import { ref } from "vue";
+import UserTree from "@/components/UserTree.vue";
+
+const users = ref([
+  {
+    name: "Chủ tịch",
+    children: [
+      {
+        name: "COD",
+        children: [
+          {
+            name: "Ban Kinh Doanh",
+            children: [
+              {
+                name: "Phòng Kinh Doanh 1",
+                children: [
+                  {
+                    name: "Group 1",
+                    children: [
+                      { name: "User 1", children: [{ name: "0" }] },
+                      { name: "User 2", children: [{ name: "0" }] },
+                    ],
+                  },
+                  {
+                    name: "Group 1",
+                    children: [
+                      { name: "User 1", children: [{ name: "0" }] },
+                      { name: "User 2", children: [{ name: "0" }] },
+                    ],
+                  },
+                  {
+                    name: "Group 2",
+                    children: [
+                      { name: "User 3", children: [{ name: "0" }] },
+                      { name: "User 4", children: [{ name: "0" }] },
+                    ],
+                  },
+                   {
+                    name: "Group 3",
+                    children: [
+                      { name: "User 3", children: [{ name: "0" }] },
+                      { name: "User 4", children: [{ name: "0" }] },
+                    ],
+                  },
+                  {
+                    name: "Group 4",
+                    children: [
+                      { name: "User 3", children: [{ name: "0" }] },
+                      { name: "User 4", children: [{ name: "0" }] },
+                    ],
+                  }
+                ],
+              },
+              {
+                name: "Phòng Kinh Doanh 2",
+                children: [
+                  {
+                    name: "Group 1",
+                    children: [
+                      { name: "User 5", children: [{ name: "0" }] },
+                      { name: "User 6", children: [{ name: "0" }] },
+                    ],
+                  },
+                  {
+                    name: "Group 2",
+                    children: [
+                      { name: "User 7", children: [{ name: "0" }] },
+                      { name: "User 8", children: [{ name: "0" }] },
+                    ],
+                  },
+                ],
+              },
+              {
+                name: "Phòng Kinh Doanh 2",
+                children: [
+                  {
+                    name: "Group 1",
+                    children: [
+                      { name: "User 5", children: [{ name: "0" }] },
+                      { name: "User 6", children: [{ name: "0" }] },
+                    ],
+                  },
+                  {
+                    name: "Group 2",
+                    children: [
+                      { name: "User 7", children: [{ name: "0" }] },
+                      { name: "User 8", children: [{ name: "0" }] },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: "Ban Kỹ Thuật",
+            children: [
+              {
+                name: "Phòng Kỹ Thuật 1",
+                children: [
+                  {
+                    name: "Group 1",
+                    children: [
+                      { name: "User 9", children: [{ name: "0" }] },
+                      { name: "User 10", children: [{ name: "0" }] },
+                    ],
+                  },
+                  {
+                    name: "Group 2",
+                    children: [
+                      { name: "User 11", children: [{ name: "0" }] },
+                      { name: "User 12", children: [{ name: "0" }] },
+                    ],
+                  },
+                ],
+              },
+              {
+                name: "Phòng Kỹ Thuật 2",
+                children: [
+                  {
+                    name: "Group 1",
+                    children: [
+                      { name: "User 13", children: [{ name: "0" }] },
+                      { name: "User 14", children: [{ name: "0" }] },
+                    ],
+                  },
+                  {
+                    name: "Group 2",
+                    children: [
+                      { name: "User 15", children: [{ name: "0" }] },
+                      { name: "User 16", children: [{ name: "0" }] },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: "Ban Vận Hành",
+            children: [
+              {
+                name: "Phòng Vận Hành 1",
+                children: [
+                  {
+                    name: "Group 1",
+                    children: [
+                      { name: "User 17" },
+                      { name: "User 18" }
+                    ]
+                  },
+                  {
+                    name: "Group 2",
+                    children: [
+                      { name: "User 19" },
+                      { name: "User 20" }
+                    ]
+                  }
+                ]
+              },
+              {
+                name: "Phòng Vận Hành 2",
+                children: [
+                  {
+                    name: "Group 1",
+                    children: [
+                      { name: "User 21" },
+                      { name: "User 22" }
+                    ]
+                  },
+                  {
+                    name: "Group 2",
+                    children: [
+                      { name: "User 23" },
+                      { name: "User 24" }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ],
+      },
+    ],
+  },
+]);
 </script>
--- Project test/Tree Diagram/src/components/UserTree.vue
+++ Project test/Tree Diagram/src/components/UserTree.vue
@@ -1,24 +1,94 @@
 <template>
-  <div class="tree relative text-center">
+  <div class="tree relative mt-10 flex flex-col w-max items-center"
+  :class="{'CEO': users[0].name === ceoName }"
+  >
     <div
-      class="node relative inline-block m-auto px-2 bg-white border border-gray-300 rounded-md"
+      class="node p-2 relative flex justify-center w-max bg-white border border-gray-300 rounded-md"
     >
       {{ users[0].name }}
     </div>
-    <div class="children relative flex justify-center gap-5 m-5">
+    <li
+      v-if="filteredChildren.length"
+      class="children relative flex justify-center gap-5"
+    >
       <UserTree
-        v-for="(child, index) in users[0].children"
+        v-for="(child, index) in filteredChildren"
         :key="index"
         :users="[child]"
+        :ceoName="ceoName"
       />
-    </div>
+    </li>
   </div>
 </template>
 
 <script setup>
+import { computed } from "vue";
+import UserTree from "./UserTree.vue";
+
 const props = defineProps({
   users: Array,
+  ceoName: String,
+});
+
+const filteredChildren = computed(() => {
+  const filterNodes = (nodes) => {
+    return nodes
+      .filter((node) => node.name !== "0")
+      .map((node) => ({
+        ...node,
+        children: node.children ? filterNodes(node.children) : [],
+      }));
+  };
+  return filterNodes(props.users[0].children || []);
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.tree::before {
+  content: "";
+  position: absolute;
+  top: -20px;
+  border-top: 1px solid #000000;
+  transform: translateX(-50%);
+  width: 100%;
+  height: 20px;
+}
+
+.tree::after {
+  content: "";
+  position: absolute;
+  top: -20px;
+  border-top: 1px solid #000000;
+  border-left: 1px solid #000000;
+  transform: translateX(50%);
+  width: 100%;
+  height: 20px;
+}
+
+.children::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 50%;
+  border-left: 1px solid black;
+  transform: translateX(-50%);
+  width: 1px;
+  height: 20px;
+}
+
+.tree:first-child::before {
+  transform: translateX(50%);
+  border-top: none;
+}
+
+.tree:last-child::after {
+  transform: translateX(50%);
+  border-top: none;
+}
+
+.CEO::before,
+.CEO::after {
+  display: none;
+}
+
+</style>
